61cf9b57f8d108800c40a8182d92315e0224ed36
souravkumar <souravkumarswain96@gmail.com>
2026-08-12 07:05:06 +0530

    Securely stored API Keys

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,7 @@
+export const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
+
 export const YOUTUBE_LOGO = 'https://www.gstatic.com/youtube/img/icons/mweb/youtube_fill/logo_with_text/v3/20px.svg';
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YOUTUBE_API_KEY;
+export const VIDEO_DETAILS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id="
+export const VIDEO_SUGGESTION_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key="+YOUTUBE_API_KEY;
+export const SEARCH_QUERY_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&regionCode=IN&q="
